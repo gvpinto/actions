67f535263442c5da21e310b69c7c9007093ede67
Glenn Pinto <gvpinto@gmail.com>
2023-05-01 18:31:21 -0400

    added version 1.0.0

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ app.use(express.json());
 
 app.get('/', (req, res, next) => {
     console.log('Request received');
-    res.send('<h1>Everything looks good</h1>');
+    res.send('<h1>Everything looks good Version 1.0.0</h1>');
 });
 
 app.listen(8080, () => {
+    console.log('Version 1.0.0');
     console.log('Application listening on port 8080');
 });
